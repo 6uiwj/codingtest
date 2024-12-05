1669eacc73913a85accc98cb4d3cb31f1a5db166
6uiwj <oterges00@gmail.com>
2024-12-05 14:40:58 +0900

    241205 BJAlgorithm

---
+++ src/Memo.java
@@ -0,0 +1,26 @@
+//import java.util.Scanner;
+//
+//interface Main{
+//    static void main(String[]s){
+//        var v = new java.util.Scanner(System.in);
+//        var b = new StringBuilder();
+//        for(
+//                int i=v.nextInt();
+//                i-->0;
+//                b.append(v.nextInt()+v.nextInt()+"\n"));
+//                System.out.print(b);
+//    }
+//}
+//
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner s = new Scanner(System.in);
+//        StringBuffer sb = new StringBuffer();
+//        int a = s.nextInt();
+//        for (int i = 0; i < a; i++) {
+//            sb.append(s.nextInt()+s.nextInt()+"\n");
+//        }
+//        System.out.print(sb);
+//    }
+//}
