d2d831573cf8c22ec10e8cf7af80e59f112a5743
6uiwj <oterges00@gmail.com>
2024-12-16 23:27:48 +0900

    241216 BJAlgorithm

---
+++ src/stepup/sprout.java
@@ -0,0 +1,20 @@
+package stepup;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class sprout {
+    public static void main(String[] args) {
+
+
+    String msg = """
+                     ,r'"7
+            r`-_   ,'  ,/
+             \\. ". L_r'
+               `~\\/
+                  |
+                  |
+            """;
+
+        System.out.println(msg);
+    }
+}
