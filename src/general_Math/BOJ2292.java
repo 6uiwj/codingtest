ef8551f695641a6ca97214443c7759cc114b44df
6uiwj <oterges00@gmail.com>
2024-12-26 23:22:03 +0900

    241226 BJAlgorithm

--- src/general_Math/BOJ2292.java
+++ src/general_Math/BOJ2292.java
@@ -1,7 +1,18 @@
 package general_Math;
 
-public class BOJ2292 {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class BOJ2292 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int r = 1;
+        int i = 1;
+
+        while(n>r) {
+            r+=6*i;
+            i++;
+        }
+        System.out.println(i);
     }
 }
