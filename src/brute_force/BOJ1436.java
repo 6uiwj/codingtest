4862d3f118528d14b36b336fdf1a110b9d2c947f
6uiwj <oterges00@gmail.com>
2025-01-09 17:05:15 +0900

    250108 BJAlgorithm

---
+++ src/brute_force/BOJ1436.java
@@ -0,0 +1,73 @@
+package brute_force;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int ans=666;
+        int count=1;
+
+        while(count<=n) {
+            System.out.println(count+" : "+ans); //59666
+            if((ans-666)%10==0) { //666으로 끝날때
+                System.out.println(ans+"if문 거침");
+                    ans += 1000; //60666
+                    String temp = String.valueOf(ans);
+                    //6이 4개이상이면
+                //**
+                    if (temp.replace("666", "").contains("6")) { //60
+                        //1의 자리가 6이면 - 6을빼주고 9까지 1씩 더함 (66606-> 66600~66609)
+                        int t = ans; //66666
+                        int d = 0;
+                        while (t%10==6 && t>999) {
+                            t=t/10;
+                            d++;
+                        }
+                        int minus=0;
+                        if(temp.replace("666", "").charAt(temp.replace("666", "").length()-1)=='6') {
+                            for (int j=0; j<d; j++) {
+                                minus = minus +6*(int)Math.pow(10,j);
+                            }
+                        ans-=minus;
+                            //**--
+                            for (int i = 0; i < Math.pow(10,d); i++) {
+                                System.out.println(count + " : " + ans);
+                                ans++;
+                                count++;
+//                                if (String.valueOf(ans).charAt(String.valueOf(ans).length() - 1) == '9') {
+//                                    System.out.println(count + " : " + ans);
+//                                    ans = ans - 3 + 1000;
+//                                    break;
+//                                }
+                            }
+                        }
+//                        } else {
+//                            for(int i = 0; i<Math.pow(9, temp.replace("666", "").length()); i++) {
+//                                System.out.println(count + " : " + ans);
+//                                ans++;
+//                            }
+//
+//                        }
+
+                    }
+            } else { //6669
+                if (String.valueOf(ans).charAt(String.valueOf(ans).length() - 1) == '9') {
+                    System.out.println("String.valueOf(ans): " + String.valueOf(ans));
+                                    ans = ans - 3 + 1000;
+                                }
+               else {
+                    ans++;
+                    System.out.println(ans+" 마지막 else문");
+                }
+            }
+            count++;
+            System.out.println("예?");
+        }
+
+
+    }
+}
---
+++ src/brute_force/BOJ1436_2.java
@@ -0,0 +1,68 @@
+package brute_force;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1436_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int ans=666;
+        int count=1;
+
+        while(count<n) {
+            System.out.println(count + " : " + ans);
+            //666으로 끝나는 수
+            if((ans-666)%10==0) { //2665666
+                ans += 1000;  //2666666
+                StringBuilder sb = new StringBuilder(String.valueOf(ans));
+                //String temp = String.valueOf(ans);
+                String word = "666";
+                //끝에 666을빼고 6을가지고 있는 수 600666
+                if (sb.delete(sb.lastIndexOf(word),sb.lastIndexOf(word)+word.length()).toString().contains("6")) {
+                    int t = ans; //(2666)666 //(6)666 //600666
+                    int d = 0; //2
+                    //끝자리가 6일 때
+                    //while (t%10==6 && t>999) {
+                    t=-666;
+                    while(String.valueOf(t).contains("666") &&t%10==6)
+                        if(!String.valueOf(t).contains("6666")) break;
+                        t=t/10; //26666 //666
+                        d++; //1
+                    }
+                    int minus=0; //temp=2666666
+                    if(sb.charAt(sb.length()-1)=='6') {
+                        for (int j=0; j<d; j++) { //0-2
+                            minus = minus +6*(int)Math.pow(10,j); //66
+                        }
+                        ans-=minus; //2666600
+                        for (int i = 0; i < Math.pow(10,d)-1; i++) { //0-9
+                            count++;
+                           System.out.println(count + " : " + ans);
+                            ans++;
+                            if(count==n-1) break;
+                        }
+                    }
+                }
+            } else {
+                StringBuilder sb = new StringBuilder(String.valueOf(ans));
+                //String t = String.valueOf(ans); //2666699
+                int i =0;//2
+                if (sb.charAt(sb.length()-1) == '9') {
+                    while(sb.charAt(sb.length()-1)=='9') {
+                        ans = ans - 3*((int)Math.pow(10,i)); //2666666
+                        sb = sb.deleteCharAt(sb.length()-1); //sb=26666
+                        i++;
+                    }
+                    ans+=1000; //2667666
+                } else {
+                    ans++;
+                }
+            }
+            if(count==n-1) break;
+            count++;
+        }
+        System.out.println(ans);
+    }
+}
