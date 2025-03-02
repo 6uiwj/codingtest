704cd9b71a871e498ed8a6112959fb10f74e372a
6uiwj <oterges00@gmail.com>
2025-03-02 23:01:37 +0900

    250302 BOJ

--- src/order/BOJ2751_3.java
+++ src/order/BOJ2751_3.java
@@ -1,18 +1,16 @@
 package order;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
-public class BOJ2751_2 {
+public class BOJ2751_3 {
     public static void main(String[] args) throws IOException {
-        BufferedInputStream bis = new BufferedInputStream(System.in);
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = readInt(bis);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
         boolean[] sort = new boolean[2000001];
         for(int i = 0; i< n; i++) {
-            int temp = readInt(bis);
+            int temp = Integer.parseInt(br.readLine());
             if(temp < 0) {
                 temp = Math.abs(temp) + 1000000;
                 sort[temp] = true;
@@ -27,20 +25,10 @@ public class BOJ2751_2 {
         for(int i = 0; i <= 1000000; i++) {
             if(sort[i]) sb.append(i+"\n");
         }
+
         bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
-    private static int readInt(BufferedInputStream bis) throws IOException {
-        int out = 0;
-        boolean isNegative = false;
 
-        while(true) {
-            int n = bis.read();
-            if(n <= 32) return isNegative ? (-1)*out : out;
-            else if(n == '-') isNegative = true;
-            else out = (out<<3) + (out<<1) + (n-'0');
-
-        }
-    }
 }
