974c34c280d87f426f41e92b2905025844f8fa20
minG <148047079+6uiwj@users.noreply.github.com>
2025-03-14 03:18:16 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13880 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String t = """
+                |\\_/|
+                |q p|   /}
+                ( 0 )""\"\\
+                |"^"`    |
+                ||_/=\\\\__|
+                """;
+
+        System.out.println(t);
+    }
+}
