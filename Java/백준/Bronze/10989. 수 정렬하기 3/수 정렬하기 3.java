dab683dc1cf6835095e3cb244a74f94308a146c8
minG <148047079+6uiwj@users.noreply.github.com>
2025-03-14 14:35:35 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1644 ms, Memory: 480800 KB -BaekjoonHub

--- Java/백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ Java/백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -2,37 +2,20 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedInputStream bis = new BufferedInputStream(System.in);
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = readInt(bis);
-        int[] sort = new int[10001];
-        for(int i = 0; i< n; i++) {
-            int temp = readInt(bis);
-            sort[temp]++;        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); 
+        int[] count = new int[10001]; 
+
+        for (int i = 0; i < n; i++) {
+            count[Integer.parseInt(br.readLine())]++;
+        }
 
         StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i <= 10000; i++) {
-            while(sort[i]!=0) {
-                sb.append(i + "\n");
-                sort[i]--;
+        for (int i = 1; i <= 10000; i++) {
+            while (count[i]-- > 0) {
+                sb.append(i).append("\n");
             }
         }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.print(sb.toString()); 
     }
-    private static int readInt(BufferedInputStream bis) throws IOException {
-        int out = 0;
-        boolean isNegative = false;
-
-        while(true) {
-            int n = bis.read();
-            if(n <= 32) return isNegative ? (-1)*out : out;
-            else if(n == '-') isNegative = true;
-            else out = (out<<3) + (out<<1) + (n-'0');
-
-        }
-    }
-
 }
