8ea3918144d76491d796ecc682dbd8449008501e
minG <148047079+6uiwj@users.noreply.github.com>
2025-12-27 18:20:39 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -3,21 +3,15 @@ class Solution {
         int sum = 0;
         int answer = 0;
 
-        for (int i = 0; i < number.length; i++) {
-            sum += number[i];
-            for(int j = i + 1; j < number.length; j++) {
-                sum += number[j];
+        for (int i = 0; i < number.length-2; i++) {
+            for(int j = i + 1; j < number.length-1; j++) {
                 for(int k = j + 1; k < number.length; k++) {
-                    sum += number[k];
-
+                    sum = number[i] + number[j] + number[k];
                     if(sum == 0) {
                         answer++;
                     }
-                    sum -= number[k];
                 }
-                sum -= number[j];
             }
-            sum = 0;
         }
 
         return answer;
