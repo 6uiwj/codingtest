1a9b2991f2f4f323624a04f0ffc007e9d0a37bf8
minG <148047079+6uiwj@users.noreply.github.com>
2026-01-04 21:27:44 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 460 ms, Memory: 41692 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,23 +1,24 @@
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedInputStream bis = new BufferedInputStream(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
+        int n = readInt();
         int[][] coords = new int[n][2];
 
         for (int i = 0; i < n; i++) {
-            String temp = br.readLine();
-            String[] coord = temp.split(" ");
-            coords[i][0] = Integer.parseInt(coord[0]);
-            coords[i][1] = Integer.parseInt(coord[1]);
+            coords[i][0] = readInt();
+            coords[i][1] = readInt();
         }
+
         Arrays.sort(coords, (a, b) -> {
             if (a[0] == b[0]) return a[1] - b[1];
             return a[0] - b[0];
@@ -27,5 +28,20 @@ public class Main {
         }
         System.out.print(sb);
     }
-    
+
+
+    private static int readInt() throws IOException {
+        int out = 0;
+        boolean isNegative = false;
+
+        while(true) {
+            int n = System.in.read();
+            if(n <= 32) return isNegative ? (-1)*out : out;
+            else if(n == '-') isNegative = true;
+            else out = (out<<3) + (out<<1) + (n-'0');
+        }
+    }
+
+
+
 }
