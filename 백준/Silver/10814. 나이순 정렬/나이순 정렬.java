2771b6bfe8b51f621d261dee3c8e30409f20e268
minG <148047079+6uiwj@users.noreply.github.com>
2026-01-18 19:20:53 +0900

    [Silver V] Title: 나이순 정렬, Time: 720 ms, Memory: 64300 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,27 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        String[] users = new String[n];
+        int idx = 0;
+        String[] users = new String[2];
+        List<Users> usersList = new ArrayList<Users>();
+        Users[] usersArray = new Users[n];
+        StringBuffer sb = new StringBuffer();
         for (int i=0; i<n; i++) {
-            users[i] = br.readLine();
+            users = br.readLine().split(" ");
+            Users user = new Users();
+            user.age = Integer.parseInt(users[0]);
+            user.name = users[1];
+            user.index = idx;
+            usersArray[i] = user;
+            idx++;
         }
-        Arrays.sort(users, (a,b)-> {
-            int subA = Integer.parseInt(a.substring(0,a.indexOf(" ")));
-            int subB = Integer.parseInt(b.substring(0,b.indexOf(" ")));
-            return subA-subB;
+
+        Arrays.sort(usersArray, (a,b) -> {
+            if (a.age != b.age) return a.age - b.age;
+            return a.index - b.index;
         });
 
-        for(String word : users) {
-            System.out.println(word);
+        for (Users user : usersArray) {
+            sb.append(user.age).append(" ").append(user.name).append("\n");
         }
-
+        System.out.println(sb.toString());
     }
+}
+
+class Users {
+    int age;
+    String name;
+    int index;
 }
