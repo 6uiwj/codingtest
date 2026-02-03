64e31fa4e462df575e85ffa3d7c2f81c135d2947
minG <148047079+6uiwj@users.noreply.github.com>
2026-02-04 00:49:48 +0900

    [Silver IV] Title: 다음 소수, Time: 192 ms, Memory: 11460 KB -BaekjoonHub

---
+++ Java/백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        long n = readLong();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i< n; i++) {
+            long m = readLong();
+            sb.append(prime(m)).append("\n");
+        }
+        System.out.println(sb);
+
+
+    }
+
+    static long prime (long num) {
+        if (num <= 2) return 2;
+
+        if (num % 2 == 0) num++;
+
+        while (true) {
+            boolean isPrime = true;
+            for (long i = 3; i * i <= num; i += 2) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) return num;
+            num += 2;
+        }
+    }
+
+    static long readLong() throws IOException {
+        long out = 0;
+        while(true) {
+            int n = System.in.read();
+            if(n <= 32) return out;
+            else out = (out << 3 ) + ( out << 1 ) + (n - '0');;
+        }
+     }
+}
