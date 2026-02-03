1c117a04d9ba0c7fbc04847f5d107a94e223eeee
minG <148047079+6uiwj@users.noreply.github.com>
2026-02-03 19:43:44 +0900

    [Silver IV] Title: 주몽, Time: 84 ms, Memory: 12716 KB -BaekjoonHub

--- Java/백준/Silver/1940. 주몽/주몽.java
+++ Java/백준/Silver/1940. 주몽/주몽.java
@@ -1,39 +1,31 @@
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int N = readInt(); 
-        int M = readInt(); 
+        int N = readInt();
+        int M = readInt();
+        int[] A = new int[N];
+        for(int i = 0; i< N; i++) {
+            A[i] = readInt();
+        }
+        Arrays.sort(A);
         int count = 0;
-        int[] nums = new int[N];
-        for(int i =0; i<N; i++) {
-            nums[i] = readInt();
-        }
-
         int i = 0;
-        int j = 1;
-        while(i<N-1) {
-            if(j>=N) {
+        int j = N-1;
+        while( i < j ) {
+            if( A[i] + A[j] < M ) i++;
+            else if ( A[i] + A[j] > M) j--;
+            else {
+                count ++;
                 i++;
-                j=i+1;
-                continue;
+                j--;
             }
-            if(nums[i] + nums[j] == M) {
-                nums[i]=-100000;
-                nums[j]=-100000;
-                count++;
-                i++;
-                j=i+1;
-                continue;
-            }
-            j++;
-
         }
-
         System.out.println(count);
-    }
 
+    }
     static int readInt() throws IOException {
         int out = 0;
         while(true) {
