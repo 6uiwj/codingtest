26a273d5ba1d970eb4b7ce0edf069e6d2ff597f6
minG <148047079+6uiwj@users.noreply.github.com>
2026-02-07 20:23:06 +0900

    [Silver III] Title: 소수 구하기, Time: 168 ms, Memory: 17972 KB -BaekjoonHub

--- Java/백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ Java/백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,29 +1,35 @@
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int N = readInt();
         int M = readInt();
+        int N = readInt();
+        boolean[] prime = new boolean[N+1];
+        Arrays.fill(prime, true);
+        prime[0] = false;
+        prime[1] = false;
         StringBuilder sb = new StringBuilder();
-        boolean prime = false;
-        for(int i = N; i <= M; i++) {
-            prime = isPrime(i);
-            if(prime) sb.append(i).append("\n");
+        for(int i = 2; i*i <= N; i++) {
+            if(prime[i]) {
+                for(int j=i*i; j<=N; j+=i) {
+                    prime[j] = false;
+                }
+            }
         }
+
+        for(int i = M; i<=N; i++) {
+            if(prime[i] == true) {
+                sb.append(i).append("\n");
+            }
+        }
+
 
         System.out.println(sb);
 
     }
 
-    private static boolean isPrime(int i) {
-        if(i==1) return false;
-        if(i==2 || i==3 ) return true;
-        for(int j = 2; j*j <=i; j++ ) {
-            if(i%j!=0) continue;
-            else return false;
-        }
-        return true;
-    }
 
     static int readInt() throws IOException {
         int out = 0;
