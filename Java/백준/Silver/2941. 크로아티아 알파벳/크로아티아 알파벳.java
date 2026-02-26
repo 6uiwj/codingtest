58596e84d911470cdf7bb0f9da343a09fbd085f9
minG <148047079+6uiwj@users.noreply.github.com>
2026-02-26 17:25:01 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 64 ms, Memory: 11652 KB -BaekjoonHub

---
+++ Java/백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+            if (a.contains("c=")) a = a.replace("c=", "*");
+            if (a.contains("c-")) a = a.replace("c-", "*");
+            if (a.contains("dz=")) a = a.replace("dz=","*");
+            if (a.contains("d-")) a = a.replace("d-", "*");
+            if (a.contains("lj")) a = a.replace("lj", "*");
+            if (a.contains("nj")) a = a.replace("nj", "*");
+            if (a.contains("s=")) a = a.replace("s=", "*");
+            if (a.contains("z=")) a = a.replace("z=", "*");
+            System.out.println(a.length());
+    }
+}
